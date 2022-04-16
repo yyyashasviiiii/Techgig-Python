a1ad363dd65a2cdef4078ffad9784627117a2dfc
YASHasvi SHUkla <72089033+YASHasvi-SHUkla@users.noreply.github.com>
2022-04-16 11:04:14 +0530

    Create String istitle.py
    You just need to take string input from stdin and checks whether all the case-based characters in the string following non-casebased letters. Non-casebased letters are uppercase and all other case-based characters are lowercase.
    
    Input Format
    You will be taking a string as an input from stdin.
    
    Constraints
    1 <= |S| <= 10000
    
    Output Format
    You need to print the boolean value(either True or False) to the stdout.
    
    Sample TestCase 1
    Input
    This Is String Example
    Output
    True

---
+++ String istitle.py
@@ -0,0 +1,15 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+
+ # Write code here 
+ a = input()
+ temp = a
+ if a.title() == temp:
+     print(True)
+ else:
+     print(False)
+
+main()
+
